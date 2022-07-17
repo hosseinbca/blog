246175d63e52aaf22b66d0c7a1cacb61b706b04c
hosseinbca <hosseinbca.prg@gmail.com>
2022-07-17 23:56:51 +0430

    user api added

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,8 +2,15 @@ from dataclasses import fields
 from pyexpat import model
 from rest_framework import serializers
 from .models import Post, User
+from django.contrib.auth import get_user_model
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id' , 'username',)
+        model = get_user_model()
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics , permissions
-from .serializers import PostSerializer
+from .serializers import PostSerializer , UserSerializer
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
+
 # Create your views here.
 
 
@@ -15,3 +17,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     permission_classes = (IsAuthorOrReadOnly,)
+    
+class UserList(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
